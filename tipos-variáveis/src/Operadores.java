484eec01696c56e15175c8769fcef72193fbd346
Perkles <gabi.clezar@gmail.com>
2023-01-17 20:08:01 -0300

    adicionando estudos

---
+++ tipos-variáveis/src/Operadores.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.print(nomeCompleto);
+    }
+        //classe operadores
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicação = 20 * 7;
+        int divisão = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        //classe operadores
+        String nome = "GABRIELLE";
+        int idade = 26;
+        double peso = 47;
+        char sexo = 'F';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+    
+    
+    
+}

f301ed744d854a5809582451343a91e1edc8025b
Perkles <gabi.clezar@gmail.com>
2023-01-17 19:27:28 -0300

    adicionando conteudo ao estudo

--- tipos-variáveis/src/TiposVariaveis.java
+++ tipos-variáveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Surto");
+
+        int numero = 10;
+
+        numero = 15;
+
+        System.out.println(numero);
+
+
+        final double VALOR_DE_PI = 3.14;
+
+        
     }
 }

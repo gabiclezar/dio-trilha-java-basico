5d286ae05f668acea4f3e52b8afa6807fd679166
Perkles <gabi.clezar@gmail.com>
2023-01-17 19:41:34 -0300

    adicionando conteudo ao estudo

--- tipos-variáveis/src/TiposVariaveis.java
+++ tipos-variáveis/src/TiposVariaveis.java
@@ -4,6 +4,15 @@ public class TiposVariaveis {
         //tipos primitivos
         // estudar a classe de String que representa texto na aplicação
 
+        byte idade = 26;
+        short ano = 2023;
+        int cep = 91000000;
+        long cpf = 85800000000L; //long usa o L no fim
+        float pi = 3.14F; //float usa o F no fim
+        double salario = 2275.55;
+        
+
+
         String meuNome = "GABI CLEZAR";
 
         int numero = 10;
